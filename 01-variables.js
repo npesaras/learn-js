1561617d1d19b606ac81ee52a99fe28cca26ac81
npesaras <nilmar.pesarast@gmail.com>
2025-12-05 23:35:34 +0800

    update variables.js

--- 01-variables.js
+++ 01-variables.js
@@ -6,12 +6,17 @@ Basic data types in JavaScript
 4. Undefined
 */
 
+
 /*
+========================================================================
+VARIABLE DECLARATIONS
+
 let - you can reassigned the value without issue
 const - you can't reassigned the value.
 
 Why should we avoid `var`?
 - var is a function-scoped, not block-scoped, which leads to unexpected behaviour
+
 */
 
 let username = "Mang Juan";
@@ -19,3 +24,25 @@ const age = 21;
 
 console.log(username); //Mang Juan
 console.log(age); //21
+
+
+/* =====================================================================
+NUMBERS
+
+Assignment: Textio tracks the number of messages sent for different types of notifications. Between the comments, create a:
+- `totalMessagesSent` variable that contains the total number of messages snet
+- `averageMessageSent` variable that contains the average number of messages sent across all types.
+
+*/
+
+const promoMessages = 12;
+const reminderMessages = 15;
+const welcomeMessages = 8;
+const supportMessages = 5;
+
+let totalMessagesSent = promoMessages + reminderMessages + welcomeMessages + supportMessages;
+
+let averageMessageSent = (totalMessagesSent)/4;
+
+console.log('Total Messages: ' + totalMessagesSent);
+console.log('Average Messages: ' + averageMessageSent);
