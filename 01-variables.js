12759f04c9bfed6f73223f53dcca1889c5c13760
npesaras <nilmar.pesarast@gmail.com>
2025-12-05 23:22:37 +0800

    update variable.js

---
+++ 01-variables.js
@@ -0,0 +1,21 @@
+/*
+Basic data types in JavaScript
+1. Number
+2. String
+3. Boolean
+4. Undefined
+*/
+
+/*
+let - you can reassigned the value without issue
+const - you can't reassigned the value.
+
+Why should we avoid `var`?
+- var is a function-scoped, not block-scoped, which leads to unexpected behaviour
+*/
+
+let username = "Mang Juan";
+const age = 21;
+
+console.log(username); //Mang Juan
+console.log(age); //21
